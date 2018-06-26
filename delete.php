4e50ff5b249ee46819b4ada43fbe87a1fbf4062d
Wilco Ram <wilcoram.spectra@gmail.com>
2018-06-26 13:01:17 +0200

    Delete function working for mutliple selections

--- delete.php
+++ delete.php
@@ -6,16 +6,20 @@ if (isset($_REQUEST["Delete"])) {
 	if ($verw=="") {
 		echo "Niks aangeklikt";
 	}else{
-	$a = explode(",", $verw[0]);
-	echo $a[0];
+	
+	for($i = 0; $i < count($verw); $i++)
+	{
+		echo count($verw);
+	$a = explode(",", $verw[$i]);
+
 	$queryDel = $conn->prepare("DELETE FROM reservering WHERE nummer = :nummer AND datum = :datum AND tijd = :tijd");
 	$queryDel->execute([
 		'nummer' => $a[0],
 		'datum' => $a[1],
 		'tijd' => $a[2]
 	]);
-	
 	}
+}
 }
 
 
