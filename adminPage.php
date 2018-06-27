5d236aabdffe12be1e1ea970eafd43286c443f20
Alysia Gode <17089581@student.hhs.nl>
2018-06-27 11:59:23 +0100

    commit rest

--- adminPage.php
+++ adminPage.php
@@ -7,6 +7,12 @@ $queryG->execute();
 $roomnames = array("DIF1.01", "DIF2.01", "DIF3.01");
 $tijden = array("9:00-10:00","10:00-11:00","11:00-12:00","12:00-13:00","13:00-14:00","14:00-15:00","15:00-16:00","16:00-17:00");
 
+if(isset($_GET['id']))
+{
+    $id = $_GET['id'];
+    session_id($id);
+    session_start();
+}
 
 ?>
 <html>
@@ -17,6 +23,7 @@ $tijden = array("9:00-10:00","10:00-11:00","11:00-12:00","12:00-13:00","13:00-14
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" type="text/css" media="screen" href="adminStyle.css" />
     <script src="adminPage.js"></script>
+    <script src="main.js"></script>
 </head>
     <body onload="startTime()">
         <form method="post" action="delete.php">
@@ -126,6 +133,50 @@ $tijden = array("9:00-10:00","10:00-11:00","11:00-12:00","12:00-13:00","13:00-14
                     <div id="tijd"></div>
                     <div id="datum"><?php echo date("l d-m-y"); ?></div>
                     </div>
+                     <!--uitloggen vakje met onclick verwijzing naar het login document-->
+                    <div id="uitloggen" type="button" onclick='loggingout("<?php print $id ?>")'>
+                        <p>Uitloggen</p>
+                    </div>
+                    
+                    <div id= "profilePic" onclick="openProfilePopup()">
+                        <p><img src="profile.png"></p>
+                    </div>  
+
+                    <div id = "naam" onclick="openProfilePopup()">
+                        <p id="usernameText"><h3><?php echo  $_SESSION["username"]; ?></h3></p>
+                    </div>
+                    <div id="profilePopup">
+                        <div id="profileAfb"><img src="anonymous.png"></div>
+                        <div id="gebruikerAdmin"><h2><?php echo "Gebruiker"; ?></h2></div>
+                        <div id="line"></div>
+                        <div id="profiledatatable">
+                            <table id="profiledata">
+                                <tr>
+                                    <td><h2>Email:</h2></td>
+                                    <td><h2><?php echo $_SESSION["email"]; ?></h2></td>
+                                </tr>
+                                <tr>
+                                    <td><h2>Naam:</h2></td>
+                                    <td><h2><?php echo $_SESSION["username"]; ?></h2></td>
+                                </tr>
+                            </table>
+                        </div>
+                        <div id="close_popup" title="Sluiten" onclick="closeProfilePopup()"><p>X</p></div>
+                    </div>
+                     <script type="text/javascript">
+                    var profilepopup = document.getElementById("profilePopup")
+
+                     function openProfilePopup()
+                    {   
+                        profilepopup.style.display = "block";
+
+                    }
+
+                    function closeProfilePopup()
+                    {
+                        profilepopup.style.display = "none";
+                    }
+            </script>
                 </div>
             </div>
         </form>
